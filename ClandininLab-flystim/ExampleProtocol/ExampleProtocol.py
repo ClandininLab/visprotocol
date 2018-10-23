d44ac282a451cd4dd02defdae98f3458fb652a85
Max Turner <mhturner@stanford.edu>
2018-10-22 17:44:45 -0700

    New leica server, client

--- ClandininLab-flystim/ExampleProtocol/ExampleProtocol.py
+++ ClandininLab-flystim/ExampleProtocol/ExampleProtocol.py
@@ -20,27 +20,18 @@ class BaseProtocol(ClandininLabProtocol.ClandininLabProtocol):
     def __init__(self):
         super().__init__()
         # # # Define your data directory # # #             
-        if platform == "darwin": #OSX (laptop, for dev.)
-            self.data_directory = '/Users/mhturner/documents/stashedObjects'
-            host = '0.0.0.0'
-            port = 60629
-            use_server = False
-        elif platform == "win32": #Windows (rig computer)
-            self.data_directory = 'E:/Max/FlystimData/'
-            host = '192.168.1.232'
-            port = 60629
-            use_server = True
+        self.data_directory = '/Users/mhturner/documents/stashedObjects'
+        host = '0.0.0.0'
+        port = 60629
 
         # # # Parameters for the screen # # # 
         # Define screen(s) for the rig you use
-        w = 14.2e-2; h = 9e-2; # meters of image at projection plane, screen only shows 9x9 of this
-        zDistToScreen = 5.36e-2; # meters
-        screens = [Screen(width=w, height=h, rotation=None, offset=(0, zDistToScreen, 0), id=0, fullscreen=False, vsync=None,
-                     square_side=2e-2, square_loc='lr')]
+        w = 15.75e-2; h = 12.6e-2; # meters of image at projection plane
+        screens = [Screen(width=w, height=h, rotation=-pi/4, offset=(5.0e-2, 6.1e-2, -6.1e-2), fullscreen=True, vsync=None, id=1)]
     
         # # # Other metadata defaults. These can be changed in the gui as well # # #
         self.experimenter = 'MyName'
-        self.rig = 'Bruker'
+        self.rig = 'Leica'
 
         # # #  List of your protocol IDs # # # 
         self.protocolIDList = ['CheckerboardWhiteNoise',
@@ -54,13 +45,7 @@ class BaseProtocol(ClandininLabProtocol.ClandininLabProtocol):
         self.indicatorChoices = ['GCaMP6f']
         
         # # # Start the stim manager and set the frame tracker square to black # # #
-        if use_server:
-            self.manager = MySocketClient(host=host, port=port)
-        else:
-            w = 15.75e-2; h = 12.6e-2; # meters of image at projection plane
-            screens = [Screen(width=w, height=h, rotation=-pi/4, offset=(5.0e-2, 6.1e-2, -6.1e-2), fullscreen=False, vsync=None)]
-            # TODO: pass screen into launch_server
-            self.manager = launch_server(flystim.stim_server, setup_name='macbook', auto_stop=True)
+        self.manager = launch_server(flystim.stim_server, setup_name='macbook', auto_stop=True)
         
         self.manager.black_corner_square()
         self.manager.set_idle_background(0)
---
+++ ClandininLab-flystim/Leica_Lcr.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from flystim.screen import Screen
+from flystim.stim_server import StimServer
+from flystim.dlpc350 import make_dlpc350_objects
+from math import pi
+
+
+def main():
+    # Put lightcrafter(s) in pattern mode
+    dlpc350_objects = make_dlpc350_objects()
+    for dlpc350_object in dlpc350_objects:
+         dlpc350_object.pattern_mode(fps=115.06)
+    
+    if len(dlpc350_objects) == 0:
+        print('No lightcrafters detected! Try sudo')   
+     
+    # Define screen(s) for the rig
+    w = 15.75e-2; h = 12.6e-2; # meters of image at projection plane
+
+    leica_lcr = Screen(width=w, height=h, rotation=-pi/4, offset=(5.0e-2, 6.1e-2, -6.1e-2), id=1, fullscreen=True, vsync=None, square_side=3.75e-2, square_loc='lr')
+    screens = [leica_lcr]
+    port = 60629
+    host = ''
+    
+    manager =  StimServer(screens=screens, host=host, port=port, auto_stop=False)
+    
+    # TODO: get these two manager commands to work?
+    manager.black_corner_square()
+    manager.set_idle_background(0)    
+    
+    manager.loop()
+    
+if __name__ == '__main__':
+    main()
