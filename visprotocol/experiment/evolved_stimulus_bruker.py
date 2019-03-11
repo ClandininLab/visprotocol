4fadb141ffd8e02ab376d87f4c7a125f04c63d9c
Max Turner <maxwellholteturner@gmail.com>
2019-03-11 16:07:13 -0700

    Bruker evolved stim, need to test...

--- visprotocol/experiment/evolved_stimulus_bruker.py
+++ visprotocol/experiment/evolved_stimulus_bruker.py
@@ -1,67 +1,110 @@
 #!/usr/bin/env python3
 
-from flystim.stim_server import launch_stim_server
-from flystim.screen import Screen
-
 from time import sleep
-import numpy as np
+import nidaqmx
+from datetime import datetime
+import flyrpc.multicall
+import os
 
 from stimvolver import stimulus_evolution
+import visprotocol.client
+import visprotocol.data
+import visprotocol.protocol
+
 
 def main():
-    pre_time = 0.05
-    stim_time = 1.0
-    tail_time = 0.05
+    protocol = visprotocol.protocol.mht_protocol.BaseProtocol()
+
+    protocol.run_parameters = {'protocol_ID':'evolved_grid_stimulus',
+              'pre_time':0.25,
+              'stim_time':1.0,
+              'tail_time':0.25,
+              'idle_color':0.5}
     
-    encoding_scheme = 'ternary_dense'
-    update_rate = 1
-    stixel_size = 5
-    num_theta = 12
-    num_phi = 8
-    t_dim = 1
-    background = 0.5
-    center_theta = 90
-    center_phi = 90
+    protocol.epoch_parameters = {'name':'ArbitraryGrid',
+                                 'encoding_scheme':'ternary_dense',
+                                 'update_rate': 1,
+                                 'stixel_size': 5,
+                                 'num_theta': 12,
+                                 'num_phi': 8,
+                                 't_dim': 1,
+                                 'background': 0.5,
+                                 'center_theta': 90,
+                                 'center_phi': 90}
+    protocol.send_ttl = False
+    series_number = 1
     
+    #GA evolver parameters:
     num_generations = 50
     num_individuals = 40
     mutation_rate = 0.005
     num_persistent_parents = 10
-
     
-    evolver = stimulus_evolution.StimulusEvolver(stim_size = (num_phi,num_theta,t_dim), 
+    
+    client = visprotocol.client.mht_client.Client()
+    multicall = flyrpc.multicall.MyMultiCall(client.manager)
+    
+
+    data = visprotocol.data.mht_data.Data()
+    data.experiment_file_name = 'test'
+    data.initializeExperimentFile()
+
+    stim_size = (protocol.epoch_parameters['num_phi'],
+                 protocol.epoch_parameters['num_theta'],
+                 protocol.epoch_parameters['t_dim'])
+    
+    evolver = stimulus_evolution.StimulusEvolver(stim_size = stim_size, 
                                                  pop_size = num_individuals, 
                                                  num_persistent_parents = num_persistent_parents,
                                                  mutation_rate = mutation_rate,
-                                                 stimulus_type = encoding_scheme, 
-                                                 response_type = 'model_RGC')
-
-    manager = launch_stim_server(Screen(fullscreen=False))
+                                                 stimulus_type = protocol.epoch_parameters['encoding_scheme'], 
+                                                 response_type = 'Bruker_bot')
+    
+    date_name = datetime.now().isoformat()[:-16].replace('-','')
+    evolver.data_directory = os.path.join('E:/Max/FlystimData', date_name)
+    evolver.series_name = 'TSeries-' + date_name + '-' + ('00' + str(series_number))[-3:]
+    evolver.pre_time = protocol.run_parameters['pre_time']
+    
+    client.manager.set_idle_background(protocol.run_parameters['idle_color'])
 
     for gen in range(num_generations):
+        
+        evolver.cycle_number = gen+1
         if gen == 0:
             evolver.initializePopulation()
-
         print('----------GEN ' + str(gen) + '/' + str(num_generations) + '---------------')
         
+        if protocol.send_ttl:
+            # Send a TTL pulse through the NI-USB to trigger acquisition
+            with nidaqmx.Task() as task:
+                task.co_channels.add_co_pulse_chan_time("Dev5/ctr0",
+                                                        low_time=0.002,
+                                                        high_time=0.001)
+                task.start()
+                
+        
         for ind in range(num_individuals):
-            if ind == 0:
-                stimulus_code = evolver.current_generation[ind,:].flatten() / 2 + 0.5
-                
-                manager.load_stim('ArbitraryGrid', stixel_size = stixel_size, 
-                      num_theta = num_theta, num_phi = num_phi, t_dim = t_dim, update_rate = update_rate, 
-                      center_theta = center_theta, center_phi = center_phi, background = background, 
-                      stimulus_code = stimulus_code.tolist(), encoding_scheme = encoding_scheme, hold = True)
-                
-                
-                sleep(pre_time)
+            stimulus_code = evolver.current_generation[ind,:].flatten() / 2 + 0.5 #convert to intensity on [0,1] from contrast [-1,1]
+            protocol.epoch_parameters['stimulus_code'] = stimulus_code.tolist()
             
-                manager.start_stim()
-                sleep(stim_time)
             
-                manager.stop_stim(print_profile = False)
-                sleep(tail_time)
+            # # # # Load and start flystim stimulus # # # # #
+            passedParameters = protocol.epoch_parameters.copy()
+            multicall.load_stim(**passedParameters)
             
+            sleep(protocol.run_parameters['pre_time'])
+            #stim time
+            multicall.start_stim()
+            multicall.start_corner_square()
+            multicall()
+            sleep(protocol.run_parameters['stim_time'])
+            
+            #tail time
+            multicall.stop_stim()
+            multicall.black_corner_square()
+            multicall()
+            sleep(protocol.run_parameters['tail_time'])     
+
         #collect responses from this generation
         
         #evolve next generation of stimuli
