4000240a7d26ff1ae06dae8592b8499ff31f4c4b
Max Turner <mhturner@stanford.edu>
2019-04-16 19:00:06 -0700

    Temp update screen coords

--- visprotocol/server/AODscope_OneScreen.py
+++ visprotocol/server/AODscope_OneScreen.py
@@ -4,21 +4,21 @@ from flystim.screen import Screen
 from flystim.stim_server import StimServer
 from flystim.dlpc350 import make_dlpc350_objects
 from math import pi
+import matplotlib.pyplot as plt
 
 
 def main():
     # Put lightcrafter(s) in pattern mode
     dlpc350_objects = make_dlpc350_objects()
     for dlpc350_object in dlpc350_objects:
-         dlpc350_object.pattern_mode(fps=115.06)
-         #dlpc350_object.pattern_mode(fps=60.0)    
+         dlpc350_object.pattern_mode(fps=115.06)  
     if len(dlpc350_objects) == 0:
         print('No lightcrafters detected! Try sudo')   
      
     # Define screen(s) for the rig
-    w = 20.6e-2; h = 12.8e-2; # meters of image at projection plane
+    w = 14.0e-2; h = 9.0e-2; # meters of image at projection plane
 
-    AODscope_left_screen = Screen(width=w, height=h, rotation=pi+pi/4, offset=(-3.9e-2, 4.0e-2, -6.1e-2), server_number=1, id=1, fullscreen=True, vsync=None, square_side=5e-2, square_loc='lr')
+    AODscope_left_screen = Screen(width=w, height=h, rotation=pi, offset=(2.5e-2, 4.5e-2, -5.5e-2), server_number=1, id=1, fullscreen=True, vsync=None, square_side=2.5e-2, square_loc='ll')
  
     aux_screen = Screen(width=w, height=h, rotation=pi+pi/4, offset=(-3.9e-2, 4.0e-2, -6.1e-2), server_number=1, id=0, fullscreen=False, vsync=None, square_side=0)
 
@@ -27,6 +27,7 @@ def main():
     host = ''
 
     AODscope_left_screen.draw()
+    plt.show()
     
     manager =  StimServer(screens=screens, host=host, port=port, auto_stop=False)
     
