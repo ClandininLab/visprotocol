7d9f9fde96181b86fb40a8c87a0b56944ff6fb62
michellepang <mmpang@stanford.edu>
2019-05-24 22:28:58 -0700

    splitting UniformFlash into BinaryFlash and MultipleContrastFlash in mmp_protocol to avoid error with the randomize checkbox (probably)

--- visprotocol/protocol/mmp_protocol.py
+++ visprotocol/protocol/mmp_protocol.py
@@ -15,7 +15,45 @@ class BaseProtocol(clandinin_protocol.BaseProtocol):
         self.parameter_preset_directory = os.path.join(inspect.getfile(visprotocol).split('visprotocol')[0], 'visprotocol', 'resources', user_name, 'parameter_presets')
         
 # %%
-class UniformFlash(BaseProtocol):
+class BinaryFlash(BaseProtocol):
+    def __init__(self):
+        super().__init__()
+        
+        self.getRunParameterDefaults()
+        self.getParameterDefaults()
+    
+    def getEpochParameters(self):
+        stimulus_ID = 'MovingPatch'
+
+        trajectory = RectangleTrajectory(x = self.protocol_parameters['center'][0],
+                                              y = self.protocol_parameters['center'][1],
+                                              angle = 0,
+                                              h = self.protocol_parameters['height'],
+                                              w = self.protocol_parameters['width'],
+                                              color = self.protocol_parameters['intensity']).to_dict()  
+
+        self.epoch_parameters = {'name':stimulus_ID,
+                            'background':self.run_parameters['idle_color'],
+                            'trajectory':trajectory}
+        self.convenience_parameters = self.protocol_parameters.copy()
+
+    def getParameterDefaults(self):
+        self.protocol_parameters = {'height':180.0,
+                       'width':180.0,
+                       'center': [90.0, 120.0],
+                       'intensity': 1.0}
+
+
+    def getRunParameterDefaults(self):
+        self.run_parameters = {'protocol_ID':'BinaryFlash',
+              'num_epochs':100,
+              'pre_time':0.25,
+              'stim_time':0.5,
+              'tail_time':0.25,
+              'idle_color':0}
+
+#%%
+class MultipleContrastFlash(BaseProtocol):
     def __init__(self):
         super().__init__()
         
@@ -49,9 +87,9 @@ class UniformFlash(BaseProtocol):
 
 
     def getRunParameterDefaults(self):
-        self.run_parameters = {'protocol_ID':'UniformFlash',
-              'num_epochs':10,
-              'pre_time':1.0,
-              'stim_time':0.5,
-              'tail_time':1.0,
+        self.run_parameters = {'protocol_ID':'MultipleContrastFlash',
+              'num_epochs':100,
+              'pre_time':0.5,
+              'stim_time':0.02,
+              'tail_time':0.5,
               'idle_color':0.5}
