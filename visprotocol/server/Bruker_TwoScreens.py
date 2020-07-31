d4ebd10fc3061be45816a773b4c99a40e455c236
Max <maxwellholteturner@gmail.com>
2020-07-31 16:49:59 -0700

    Update bruker 2 screens

--- visprotocol/server/Bruker_TwoScreens.py
+++ visprotocol/server/Bruker_TwoScreens.py
@@ -12,18 +12,37 @@ def main():
     dlpc350_objects = make_dlpc350_objects()
     for dlpc350_object in dlpc350_objects:
          dlpc350_object.pattern_mode(fps=120)
-<<<<<<< HEAD
-
-=======
          dlpc350_object.pattern_mode(fps=120)
-     
->>>>>>> 0e908def370a97c5b830233c7b9e9fa338abc167
-    w = 20.6e-2; h = 12.8e-2; # meters of image at projection plane
 
-    bruker_right_screen = Screen(width=w, height=h, rotation=pi/4, offset=(3.0e-2, -4.4e-2, -5.8e-2), id=2, fullscreen=True, vsync=True, square_size=(0.2, 0.2), square_loc=(-1.0, -1.0))
-    bruker_left_screen = Screen(width=w, height=h, rotation=pi-pi/4, offset=(4.0e-2, 3.9e-2, -6.1e-2), id=1, fullscreen=True, vsync=True, square_size=(0.2, 0.2), square_loc=(0.8, -1.0))
+    # Define screen(s) for the rig. Units in meters
+    # Fly is at (0, 0, 0), fly looking down +y axis. Top of the screen is at z=0
+    z_bottom = -12.13e-2 #m
+    x_left = -7.99e-2
+    x_right = +7.99e-2
+    y_forward = +7.17e-2
+    y_back = -0.8e-2
 
-    aux_screen = Screen(width=w, height=h, rotation=pi-pi/4, offset=(4.0e-2, 3.9e-2, -6.1e-2), id=0, fullscreen=False, vsync=True, square_size=(0.0, 0.0))
+    # LEFT SCREEN
+    left_pa = ((-0.55, -1.0), (x_left, y_back, z_bottom))
+    left_pb = ((+0.65, -1.0), (0, y_forward, z_bottom))
+    left_pc = ((-0.55, +1.0), (x_left, y_back, 0))
+    left_p4 = ((+0.65, +1.0), (0, y_forward, 0))
+
+    left_tri = Screen.quad_to_tri_list(left_pa, left_pb, left_pc, left_p4)
+
+    bruker_left_screen = Screen(tri_list=left_tri, id=0, fullscreen=False, vsync=True, square_size=(0.18, 0.25), square_loc=(0.78, -0.86), name='Left', horizontal_flip=True)
+
+    # RIGHT SCREEN
+    right_pa = ((-0.57, -1.0), (0, y_forward, z_bottom))
+    right_pb = ((+0.60, -1.0), (x_right, y_back, z_bottom))
+    right_pc = ((-0.54, +0.96), (0, y_forward, 0))
+    right_p4 = ((+0.62, +0.94), (x_right, y_back, 0))
+
+    right_tri = Screen.quad_to_tri_list(right_pa, right_pb, right_pc, right_p4)
+
+    bruker_right_screen = Screen(tri_list=right_tri, id=0, fullscreen=False, vsync=True, square_size=(0.18, 0.25), square_loc=(-0.88, -0.92), name='Right', horizontal_flip=True)
+
+    aux_screen = Screen(tri_list=left_tri, id=0, fullscreen=False, vsync=True, square_size=(0, 0), square_loc=(-1, -1), name='Aux', horizontal_flip=False)
 
     draw_screens([bruker_left_screen, bruker_right_screen])
     plt.show()
