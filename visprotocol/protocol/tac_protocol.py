8ba8fdbdbdf9e54f9ff2f449341f7144b88c8c05
Tim Currier <tcurrier@Tims-MacBook-Pro.local>
2020-12-13 13:50:37 -0800

    adjustments to default search stimulus in tac_protocol

--- visprotocol/protocol/tac_protocol.py
+++ visprotocol/protocol/tac_protocol.py
@@ -33,11 +33,11 @@ class SearchStimulus(BaseProtocol):
         self.convenience_parameters = {'current_intensity': current_intensity}
 
     def getParameterDefaults(self):
-        self.protocol_parameters = {'height': 120.0,
-                                    'width': 120.0,
+        self.protocol_parameters = {'height': 20.0,
+                                    'width': 20.0,
                                     'center': [0, 0],
                                     'intensity': [1.0, 0.0],
-                                    'randomize_order': True}
+                                    'randomize_order': False}
 
     def getRunParameterDefaults(self):
         self.run_parameters = {'protocol_ID': 'SearchStimulus',
@@ -47,51 +47,4 @@ class SearchStimulus(BaseProtocol):
                                'tail_time': 1.0,
                                'idle_color': 0.5}
 
-# %%
 
-
-class DriftingSquareGrating(BaseProtocol):
-    def __init__(self, cfg):
-        super().__init__(cfg)
-
-        self.getRunParameterDefaults()
-        self.getParameterDefaults()
-
-    def getEpochParameters(self):
-        current_angle = self.selectParametersFromLists(self.protocol_parameters['angle'], randomize_order = self.protocol_parameters['randomize_order'])
-
-        self.epoch_parameters = {'name': 'RotatingGrating',
-                                 'period': self.protocol_parameters['period'],
-                                 'rate': self.protocol_parameters['rate'],
-                                 'color': [1, 1, 1, 1],
-                                 'mean': self.protocol_parameters['mean'],
-                                 'contrast': self.protocol_parameters['contrast'],
-                                 'angle': current_angle,
-                                 'offset': 0.0,
-                                 'cylinder_radius': 1,
-                                 'cylinder_height': 10,
-                                 'profile': 'square',
-                                 'theta': self.screen_center[0]}
-
-        self.convenience_parameters = {'current_angle': current_angle}
-
-        self.meta_parameters = {'center_size': self.protocol_parameters['center_size'],
-                                'center': self.adjustCenter(self.protocol_parameters['center'])}
-
-    def getParameterDefaults(self):
-        self.protocol_parameters = {'period': 20.0,
-                                    'rate': 20.0,
-                                    'contrast': 1.0,
-                                    'mean': 0.5,
-                                    'angle': [0.0, 90.0, 180.0, 270.0],
-                                    'center': [0, 0],
-                                    'center_size': 180.0,
-                                    'randomize_order': True}
-
-    def getRunParameterDefaults(self):
-        self.run_parameters = {'protocol_ID': 'DriftingSquareGrating',
-                               'num_epochs': 10,
-                               'pre_time': 1.0,
-                               'stim_time': 4.0,
-                               'tail_time': 1.0,
-                               'idle_color': 0.5}
