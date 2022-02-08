1c475cb856c1de53759c2550e648d7d82ce38306
Minseung Choi <minseung@stanford.edu>
2022-02-08 14:44:20 -0800

    Double shifted cylinders for optic flow for Luke

--- visprotocol/protocol/mc_protocol.py
+++ visprotocol/protocol/mc_protocol.py
@@ -491,6 +491,84 @@ class DriftingSquareGrating(BaseProtocol):
 
 # %%
 
+class SplitDriftingSquareGrating(BaseProtocol):
+    def __init__(self, cfg):
+        super().__init__(cfg)
+
+        self.getRunParameterDefaults()
+        self.getParameterDefaults()
+
+    def loadStimuli(self, client):
+        passed_parameters_0 = self.epoch_parameters_0.copy()
+        passed_parameters_1 = self.epoch_parameters_1.copy()
+    
+        bg = self.run_parameters.get('idle_color')
+        multicall = flyrpc.multicall.MyMultiCall(client.manager)
+        multicall.load_stim(name='ConstantBackground', color=[bg,bg,bg,1], side_length=200)
+        multicall.load_stim(**passed_parameters_0, hold=True)
+        multicall.load_stim(**passed_parameters_1, hold=True)
+        multicall()
+
+    def getEpochParameters(self):
+        # TODO: center size with aperture (center and center_size)
+        current_angle = self.selectParametersFromLists(self.protocol_parameters['angle'], randomize_order = self.protocol_parameters['randomize_order'])
+
+        self.epoch_parameters_0 = {'name': 'RotatingGrating',
+                                 'period': self.protocol_parameters['period'],
+                                 'rate': self.protocol_parameters['rate'],
+                                 'color': [1, 1, 1, 1],
+                                 'mean': self.protocol_parameters['mean'],
+                                 'contrast': self.protocol_parameters['contrast'],
+                                 'angle': current_angle,
+                                 'offset': 0.0,
+                                 'cylinder_radius': 1,
+                                 'cylinder_location': (self.protocol_parameters['cylinder_xshift'],0,0),
+                                 'cylinder_height': 10,
+                                 'profile': 'square',
+                                 'theta': self.screen_center[0]}
+        self.epoch_parameters_1 = {'name': 'RotatingGrating',
+                                 'period': self.protocol_parameters['period'],
+                                 'rate': self.protocol_parameters['rate'], #-
+                                 'color': [1, 1, 1, 1],
+                                 'mean': self.protocol_parameters['mean'],
+                                 'contrast': self.protocol_parameters['contrast'],
+                                 'angle': current_angle+180.0, #remove
+                                 'offset': 0.0, #change this??
+                                 'cylinder_radius': 1,
+                                 'cylinder_location': (self.protocol_parameters['cylinder_xshift'],0,0), #-
+                                 'cylinder_height': 10,
+                                 'profile': 'square',
+                                 'theta': self.screen_center[0]}
+        # self.epoch_parameters_1 = self.epoch_parameters_0.copy()
+        # self.epoch_parameters_1['cylinder_location'] = (-self.protocol_parameters['cylinder_xshift'],0,0)
+
+
+        self.convenience_parameters = {'current_angle': current_angle}
+
+        self.meta_parameters = {'center_size': self.protocol_parameters['center_size'],
+                                'center': self.adjustCenter(self.protocol_parameters['center'])}
+
+    def getParameterDefaults(self):
+        self.protocol_parameters = {'period': 20.0,
+                                    'rate': 20.0,
+                                    'contrast': 1.0,
+                                    'mean': 0.5,
+                                    'angle': [0.0, 180.0],
+                                    'center': [0, 0],
+                                    'center_size': 180.0,
+                                    'cylinder_xshift': -0.001,
+                                    'randomize_order': True,
+                                    }
+
+    def getRunParameterDefaults(self):
+        self.run_parameters = {'protocol_ID': 'SplitDriftingSquareGrating',
+                               'num_epochs': 40,
+                               'pre_time': 1.0,
+                               'stim_time': 4.0,
+                               'tail_time': 1.0,
+                               'idle_color': 0.5}
+
+# %%
 
 class ExpandingMovingSpot(BaseProtocol):
     def __init__(self, cfg):
