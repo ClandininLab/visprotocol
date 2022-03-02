9da27624e6ed16c05923e816d4c5e988b608b575
Minseung Choi <minseung@stanford.edu>
2022-03-02 09:02:38 -0800

    ExpandingEdges added to mc_protocol

--- visprotocol/protocol/mc_protocol.py
+++ visprotocol/protocol/mc_protocol.py
@@ -629,6 +629,57 @@ class SphericalCheckerboardWhiteNoise(BaseProtocol):
 
 # %%
 
+class ExpandingEdges(BaseProtocol):
+    def __init__(self, cfg):
+        super().__init__(cfg)
+
+        self.getRunParameterDefaults()
+        self.getParameterDefaults()
+
+    def getEpochParameters(self):
+        current_rate, current_expand_dark = self.selectParametersFromLists((self.protocol_parameters['rate'], self.protocol_parameters['expand_dark']), randomize_order = self.protocol_parameters['randomize_order'])
+
+
+        self.epoch_parameters = {'name': 'ExpandingEdges',
+                                 'period': self.protocol_parameters['period'],
+                                 'rate': current_rate,
+                                 'expander_color': self.protocol_parameters['dark_color'] if current_expand_dark else self.protocol_parameters['light_color'],
+                                 'opposite_color': self.protocol_parameters['light_color'] if current_expand_dark else self.protocol_parameters['dark_color'],
+                                 'width_0': self.protocol_parameters['width_0'],
+                                 'hold_duration': self.protocol_parameters['hold_duration'],
+                                 'color': [1, 1, 1, 1],
+                                 'n_theta_pixels': self.protocol_parameters['n_theta_pixels'],
+                                 'cylinder_radius': 1,
+                                 'vert_extent': self.protocol_parameters['vert_extent'],
+                                 'theta_offset': self.protocol_parameters['theta_offset'],
+                                 'theta': self.screen_center[0]}
+
+        self.convenience_parameters = {'current_rate': current_rate, 'current_expand_dark': current_expand_dark}
+
+        self.meta_parameters = {'center': self.adjustCenter(self.protocol_parameters['center'])}
+
+    def getParameterDefaults(self):
+        self.protocol_parameters = {'period': 40.0,
+                                    'rate': [-80.0, 80.0],
+                                    'vert_extent': 80.0,
+                                    'theta_offset': 0.0,
+                                    'light_color': 1.0,
+                                    'dark_color': 0.0,
+                                    'expand_dark': [0,1],
+                                    'width_0': 2,
+                                    'hold_duration': 0.550,
+                                    'n_theta_pixels': 5760,
+                                    'center': [0, 0],
+                                    'randomize_order': True}
+
+    def getRunParameterDefaults(self):
+        self.run_parameters = {'protocol_ID': 'ExpandingEdges',
+                               'num_epochs': 400,
+                               'pre_time': 1.0,
+                               'stim_time': 0.800, # 0.550 hold then 0.250 rotate
+                               'tail_time': 1.0,
+                               'idle_color': 0.5}
+
 
 class DriftingSquareGrating(BaseProtocol):
     def __init__(self, cfg):
