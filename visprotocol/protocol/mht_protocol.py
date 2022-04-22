6a7a1e8bdb0d6c3205d689349408a9a3c8af91a2
Max Turner <maxwellholteturner@gmail.com>
2022-04-22 09:54:21 -0700

    surround grating tuning

--- visprotocol/protocol/mht_protocol.py
+++ visprotocol/protocol/mht_protocol.py
@@ -9,7 +9,6 @@ import numpy as np
 import os
 import flyrpc.multicall
 import inspect
-import time
 
 import visprotocol
 from visprotocol.protocol import clandinin_protocol
@@ -18,124 +17,6 @@ from visprotocol.protocol import clandinin_protocol
 class BaseProtocol(clandinin_protocol.BaseProtocol):
     def __init__(self, cfg):
         super().__init__(cfg)  # call the parent class init method
-
-    def getMovingPatchParameters(self, center=None, angle=None, speed=None, width=None, height=None, color=None, distance_to_travel=None):
-        if center is None: center = self.adjustCenter(self.protocol_parameters['center'])
-        if angle is None: angle = self.protocol_parameters['angle']
-        if speed is None: speed = self.protocol_parameters['speed']
-        if width is None: width = self.protocol_parameters['width']
-        if height is None: height = self.protocol_parameters['height']
-        if color is None: color = self.protocol_parameters['color']
-
-        centerX = center[0]
-        centerY = center[1]
-        stim_time = self.run_parameters['stim_time']
-        if distance_to_travel is None:  # distance_to_travel is set by speed and stim_time
-            distance_to_travel = speed * stim_time
-            # trajectory just has two points, at time=0 and time=stim_time
-            startX = (0, centerX - np.cos(np.radians(angle)) * distance_to_travel/2)
-            endX = (stim_time, centerX + np.cos(np.radians(angle)) * distance_to_travel/2)
-            startY = (0, centerY - np.sin(np.radians(angle)) * distance_to_travel/2)
-            endY = (stim_time, centerY + np.sin(np.radians(angle)) * distance_to_travel/2)
-            x = [startX, endX]
-            y = [startY, endY]
-
-        else:  # distance_to_travel is specified, so only go that distance at the defined speed. Hang pre- and post- for any extra stim time
-            travel_time = distance_to_travel / speed
-            if travel_time > stim_time:
-                print('Warning: stim_time is too short to show whole trajectory at this speed!')
-                hang_time = 0
-            else:
-                hang_time = (stim_time - travel_time)/2
-
-            # split up hang time in pre and post such that trajectory always hits centerX,centerY at stim_time/2
-            x_1 = (0, centerX - np.cos(np.radians(angle)) * distance_to_travel/2)
-            x_2 = (hang_time, centerX - np.cos(np.radians(angle)) * distance_to_travel/2)
-            x_3 = (hang_time+travel_time, centerX + np.cos(np.radians(angle)) * distance_to_travel/2)
-            x_4 = (hang_time+travel_time+hang_time, centerX + np.cos(np.radians(angle)) * distance_to_travel/2)
-
-            y_1 = (0, centerY - np.sin(np.radians(angle)) * distance_to_travel/2)
-            y_2 = (hang_time, centerY - np.sin(np.radians(angle)) * distance_to_travel/2)
-            y_3 = (hang_time+travel_time, centerY + np.sin(np.radians(angle)) * distance_to_travel/2)
-            y_4 = (hang_time+travel_time+hang_time, centerY + np.sin(np.radians(angle)) * distance_to_travel/2)
-
-            x = [x_1, x_2, x_3, x_4]
-            y = [y_1, y_2, y_3, y_4]
-
-        x_trajectory = {'name': 'tv_pairs',
-                        'tv_pairs': x,
-                        'kind': 'linear'}
-        y_trajectory = {'name': 'tv_pairs',
-                        'tv_pairs': y,
-                        'kind': 'linear'}
-
-        patch_parameters = {'name': 'MovingPatch',
-                            'width': width,
-                            'height': height,
-                            'color': color,
-                            'theta': x_trajectory,
-                            'phi': y_trajectory,
-                            'angle': angle}
-        return patch_parameters
-
-
-    def getMovingSpotParameters(self, center=None, angle=None, speed=None, radius=None, color=None, distance_to_travel=None):
-        if center is None: center = self.protocol_parameters['center']
-        if angle is None: angle = self.protocol_parameters['angle']
-        if speed is None: speed = self.protocol_parameters['speed']
-        if radius is None: radius = self.protocol_parameters['radius']
-        if color is None: color = self.protocol_parameters['color']
-
-        center = self.adjustCenter(center)
-
-        centerX = center[0]
-        centerY = center[1]
-        stim_time = self.run_parameters['stim_time']
-        if distance_to_travel is None:  # distance_to_travel is set by speed and stim_time
-            distance_to_travel = speed * stim_time
-            # trajectory just has two points, at time=0 and time=stim_time
-            startX = (0, centerX - np.cos(np.radians(angle)) * distance_to_travel/2)
-            endX = (stim_time, centerX + np.cos(np.radians(angle)) * distance_to_travel/2)
-            startY = (0, centerY - np.sin(np.radians(angle)) * distance_to_travel/2)
-            endY = (stim_time, centerY + np.sin(np.radians(angle)) * distance_to_travel/2)
-            x = [startX, endX]
-            y = [startY, endY]
-
-        else:  # distance_to_travel is specified, so only go that distance at the defined speed. Hang pre- and post- for any extra stim time
-            travel_time = distance_to_travel / speed
-            if travel_time > stim_time:
-                print('Warning: stim_time is too short to show whole trajectory at this speed!')
-                hang_time = 0
-            else:
-                hang_time = (stim_time - travel_time)/2
-
-            # split up hang time in pre and post such that trajectory always hits centerX,centerY at stim_time/2
-            x_1 = (0, centerX - np.cos(np.radians(angle)) * distance_to_travel/2)
-            x_2 = (hang_time, centerX - np.cos(np.radians(angle)) * distance_to_travel/2)
-            x_3 = (hang_time+travel_time, centerX + np.cos(np.radians(angle)) * distance_to_travel/2)
-            x_4 = (hang_time+travel_time+hang_time, centerX + np.cos(np.radians(angle)) * distance_to_travel/2)
-
-            y_1 = (0, centerY - np.sin(np.radians(angle)) * distance_to_travel/2)
-            y_2 = (hang_time, centerY - np.sin(np.radians(angle)) * distance_to_travel/2)
-            y_3 = (hang_time+travel_time, centerY + np.sin(np.radians(angle)) * distance_to_travel/2)
-            y_4 = (hang_time+travel_time+hang_time, centerY + np.sin(np.radians(angle)) * distance_to_travel/2)
-
-            x = [x_1, x_2, x_3, x_4]
-            y = [y_1, y_2, y_3, y_4]
-
-        x_trajectory = {'name': 'tv_pairs',
-                        'tv_pairs': x,
-                        'kind': 'linear'}
-        y_trajectory = {'name': 'tv_pairs',
-                        'tv_pairs': y,
-                        'kind': 'linear'}
-
-        spot_parameters = {'name': 'MovingSpot',
-                           'radius': radius,
-                           'color': color,
-                           'theta': x_trajectory,
-                           'phi': y_trajectory}
-        return spot_parameters
 
 # %%
 
@@ -549,7 +430,6 @@ class SurroundGratingTuning(BaseProtocol):
         grate_parameters = self.epoch_parameters[0].copy()
         patch_parameters = self.epoch_parameters[1].copy()
 
-        bg = self.run_parameters.get('idle_color')
         multicall = flyrpc.multicall.MyMultiCall(client.manager)
         multicall.load_stim(**grate_parameters, hold=True)
         multicall.load_stim(**patch_parameters, hold=True)
@@ -559,17 +439,16 @@ class SurroundGratingTuning(BaseProtocol):
         self.protocol_parameters = {'center': [0, 0],
                                     'spot_radius': 7.5,
                                     'spot_color': 0.0,
-                                    'spot_speed': [60.0],
-                                    'grate_period': [10.0, 20.0, 30.0, 40.0],
-                                    'grate_rate': [-120.0, -90.0, -60.0, -30.0, -15.0, 0.0,
-                                                   15.0, 30.0, 60.0, 90.0, 120.0],
+                                    'spot_speed': [-100, 100],
+                                    'grate_period': [5, 10, 20, 40],
+                                    'grate_rate': [20, 40, 80, 160, 320],
                                     'grate_contrast': 0.5,
                                     'angle': 0.0,
                                     'randomize_order': True}
 
     def getRunParameterDefaults(self):
         self.run_parameters = {'protocol_ID': 'SurroundGratingTuning',
-                               'num_epochs': 220, # 11 x 5 avgs each
+                               'num_epochs': 200,  # 2 x 4 x 5 = 40; 5 averages each
                                'pre_time': 1.0,
                                'stim_time': 3.0,
                                'tail_time': 1.0,
