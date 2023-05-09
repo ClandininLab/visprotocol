ede519961dd60433022cc8501619f5a923bb3766
Minseung Choi <minseung@stanford.edu>
2023-05-08 22:56:29 -0700

    Only allow parameter update when on Standby; saving preset reloads presets and selects the saved preset on GUI

--- visprotocol/client.py
+++ visprotocol/client.py
@@ -106,8 +106,6 @@ class BaseClient():
 
         self.manager.print_on_server('Run ended.')
 
-        protocol_object.prepare_run(recompute_epoch_parameters=True)
-
     def start_epoch(self, protocol_object, data, save_metadata_flag=True):
         #  get stimulus parameters for this epoch
         if protocol_object.use_precomputed_epoch_parameters:
